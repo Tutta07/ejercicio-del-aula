ef126301a5e1c3efd1cb9954ceacccc7c2892d1e
USER <baronelismy@gmail.com>
2019-08-28 06:53:12 -0300

    Agrege las fotos, hice el carrusel y el main

---
+++ main.js
@@ -0,0 +1,27 @@
+/*function carrousel(){
+    var carrousel=document.getElementById('carrousel');
+    var numcarrousel=document.getElementsByClassName('carrousel').length;
+    var btn_sig=document.getElementsByClassName('siguiente');
+    var btn_ant=document.getElementsByClassName('anterior');
+    for(i=0;i< numcarrousel-1;i++)
+    {
+      function botonSiguiente(i){
+          btn_sig[i].addEventListener("click",function(){
+carrousel.style.top=-600 *(i+1)+ "px";
+          });
+          botonSiguiente(i);
+
+      }
+    }
+    for(i=1;i< numcarrousel-1;i++)
+    {
+      function botonAnterior(i){
+          btn_ant[i].addEventListener("click",function(){
+carrousel.style.top=-600 *(i-1)+ "px";
+          });
+          botonAnterior(i);
+
+      }
+    }
+}
+carrousel();*/
