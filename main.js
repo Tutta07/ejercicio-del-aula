a0e9c2b9d9d44bb2e1e4b4e1bb51e109d5c6b9f5
USER <baronelismy@gmail.com>
2019-09-06 17:31:42 -0300

    nuevo css y una pagina nueva carrousel

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $('.carousel').carousel({
   interval: 4000
 })
+
+var inputs=document.getElementsByClassName('formulario_input');
+for(var i=0;i<inputs.length;i++){
+inputs[i].addEventListener('keyup',function(){
+  if(this.value.length>=1){
+    this.nextElementSibling.classList.add('fixar');
+  }
+  else{
+    this.nextElementSibling.classList.remove('fixar');
+  }
+});
+}
+
